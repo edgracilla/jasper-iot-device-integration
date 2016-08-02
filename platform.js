b5b04ac5784ccb945e56847e711f30a6141ecd58
Benj Sicam <benj@codechef.io>
2016-08-02 15:21:40 +0800

    Updated device integration api spec

--- platform.js
+++ platform.js
@@ -6,7 +6,7 @@
  * @returns {boolean}
  */
 var isString = function (val) {
-    return typeof val === 'string' || ((!!val && typeof val === 'object') && Object.prototype.toString.call(val) === '[object String]');
+	return typeof val === 'string' || ((!!val && typeof val === 'object') && Object.prototype.toString.call(val) === '[object String]');
 };
 
 /**
@@ -15,7 +15,7 @@ var isString = function (val) {
  * @returns {boolean}
  */
 var isError = function (val) {
-    return (!!val && typeof val === 'object') && typeof val.message === 'string' && Object.prototype.toString.call(val) === '[object Error]';
+	return (!!val && typeof val === 'object') && typeof val.message === 'string' && Object.prototype.toString.call(val) === '[object Error]';
 };
 
 /**
@@ -24,10 +24,10 @@ var isError = function (val) {
  * @constructor
  */
 function Platform() {
-    if (!(this instanceof Platform)) return new Platform();
+	if (!(this instanceof Platform)) return new Platform();
 
-    require('events').EventEmitter.call(this);
-    Platform.init.call(this);
+	require('events').EventEmitter.call(this);
+	Platform.init.call(this);
 }
 
 require('util').inherits(Platform, require('events').EventEmitter);
@@ -36,83 +36,106 @@ require('util').inherits(Platform, require('events').EventEmitter);
  * Init function for Platform.
  */
 Platform.init = function () {
-    ['SIGHUP', 'SIGINT', 'SIGTERM'].forEach((signal) => {
-        process.on(signal, () => {
-        console.log(`Executing ${signal} listener...`);
-            this.emit('close');
+	['SIGHUP', 'SIGINT', 'SIGTERM'].forEach((signal) => {
+		process.on(signal, () => {
+			console.log(`Executing ${signal} listener...`);
+			this.emit('close');
 
-            setTimeout(() => {
-                this.removeAllListeners();
-                process.exit();
-            }, 2000);
-        });
-    });
+			setTimeout(() => {
+				this.removeAllListeners();
+				process.exit();
+			}, 2000);
+		});
+	});
 
-    ['unhandledRejection', 'uncaughtException'].forEach((exceptionEvent) => {
-            process.on(exceptionEvent, (error) => {
-            console.error(exceptionEvent, error);
-            this.handleException(error);
-            this.emit('close');
+	['unhandledRejection', 'uncaughtException'].forEach((exceptionEvent) => {
+		process.on(exceptionEvent, (error) => {
+			console.error(exceptionEvent, error);
+			this.handleException(error);
+			this.emit('close');
 
-            setTimeout(() => {
-                this.removeAllListeners();
-                process.exit(1);
-            }, 2000);
-        });
-    });
+			setTimeout(() => {
+				this.removeAllListeners();
+				process.exit(1);
+			}, 2000);
+		});
+	});
 
-    /**
-    *   Available events triggered by the main instance ['ready', 'close', 'adddevice', 'updatedevice', 'removedevice']
-    */
-    process.on('message', (m) => {
-        let type = m.type;
+	/**
+	 *   Available events triggered by the main instance ['ready', 'close', 'adddevice', 'updatedevice', 'removedevice']
+	 */
+	process.on('message', (m) => {
+		let type = m.type;
 
-        switch (type) {
-            case 'ready':
-                this.emit(type, m.data.options);
-                break;
-            case 'close':
-                this.emit(type);
-                break;
-            case 'sync':
-                this.emit(type, m.data.last_sync_dt);
-                break;
-            default:
-                this.emit(type, m.data);
-        }
-    });
+		switch (type) {
+			case 'ready':
+				this.emit(type, m.data.options);
+				break;
+			case 'close':
+				this.emit(type);
+				break;
+			case 'sync':
+				this.emit(type, m.data.last_sync_dt);
+				break;
+			default:
+				this.emit(type, m.data);
+		}
+	});
 };
 
 /**
  * Add or update 3rd party service devices to Reekoh devices.
- * @param {object} [device] Device metadata.
+ * @param {string} device Device information.
  * @param {function} [callback] Optional callback to be called once the signal has been sent.
  */
-Platform.prototype.syncDevice = function(device, callback) {
-    callback = callback || function () {};
+Platform.prototype.syncDevice = function (device, callback) {
+	callback = callback || function () {
+		};
 
-    setImmediate(() => {
-        process.send({
-            type: 'upsertdevice',
-            data: device
-        }, callback);
-    });
+	setImmediate(() => {
+		if (!device || !isString(device)) return callback(new Error('Device data must be represented as a serialized JSON String.'));
+
+		process.send({
+			type: 'upsertdevice',
+			data: device
+		}, callback);
+	});
 };
 
+/**
+ * Remove device from Reekoh devices collection.
+ * @param {string} deviceId Device indentifier.
+ * @param {function} [callback] Optional callback to be called once the signal has been sent.
+ */
+Platform.prototype.removeDevice = function (deviceId, callback) {
+	callback = callback || function () {
+		};
+
+	setImmediate(() => {
+		if (!deviceId || !isString(deviceId)) return callback(new Error('A valid client/device identifier is required.'));
+
+		process.send({
+			type: 'removedevice',
+			data: {
+				_id: deviceId
+			}
+		}, callback);
+	});
+};
 
 /**
  * Needs to be called once in order to notify the platform that the plugin has already finished the init process.
  * @param {function} [callback] Optional callback to be called once the ready signal has been sent.
  */
 Platform.prototype.notifyReady = function (callback) {
-    callback = callback || function () {
-        };
+	callback = callback || function () {
+		};
 
-    setImmediate(() => {
-        process.send({
-            type: 'ready'
-        }, callback);
-    });
+	setImmediate(() => {
+		process.send({
+			type: 'ready'
+		}, callback);
+	});
 };
 
 /**
@@ -120,14 +143,14 @@ Platform.prototype.notifyReady = function (callback) {
  * @param {function} [callback] Optional callback to be called once the close signal has been sent.
  */
 Platform.prototype.notifyClose = function (callback) {
-    callback = callback || function () {
-        };
+	callback = callback || function () {
+		};
 
-    setImmediate(() => {
-        process.send({
-            type: 'close'
-        }, callback);
-    });
+	setImmediate(() => {
+		process.send({
+			type: 'close'
+		}, callback);
+	});
 };
 
 /**
@@ -136,15 +159,15 @@ Platform.prototype.notifyClose = function (callback) {
  * @param {function} callback Optional callback to be called once the data has been sent.
  */
 Platform.prototype.log = function (data, callback) {
-    callback = callback || function () {
-        };
+	callback = callback || function () {
+		};
 
-    if (!data || !isString(data)) return callback(new Error('A valid log data is required.'));
+	if (!data || !isString(data)) return callback(new Error('A valid log data is required.'));
 
-    process.send({
-        type: 'log',
-        data: data
-    }, callback);
+	process.send({
+		type: 'log',
+		data: data
+	}, callback);
 };
 
 /**
@@ -153,21 +176,21 @@ Platform.prototype.log = function (data, callback) {
  * @param {function} callback Optional callback to be called once the error has been sent.
  */
 Platform.prototype.handleException = function (error, callback) {
-    callback = callback || function () {
-        };
+	callback = callback || function () {
+		};
 
-    setImmediate(() => {
-        if (!isError(error)) return callback(new Error('A valid error object is required.'));
+	setImmediate(() => {
+		if (!isError(error)) return callback(new Error('A valid error object is required.'));
 
-        process.send({
-            type: 'error',
-            data: {
-                name: error.name,
-                message: error.message,
-                stack: error.stack
-            }
-        }, callback);
-    });
+		process.send({
+			type: 'error',
+			data: {
+				name: error.name,
+				message: error.message,
+				stack: error.stack
+			}
+		}, callback);
+	});
 };
 
 module.exports = new Platform();
--- test/device-integration.test.js
+++ test/device-integration.test.js
@@ -1,55 +1,60 @@
 'use strict';
 
 var cp     = require('child_process'),
-    assert = require('assert'),
-    deviceIntegration;
+	assert = require('assert'),
+	deviceIntegration;
 
 describe('Device-integration', function () {
-    this.slow(5000);
+	this.slow(8000);
 
-    after('terminate child process', function () {
-        deviceIntegration.kill('SIGKILL');
-    });
+	after('terminate child process', function () {
+		deviceIntegration.kill('SIGKILL');
+	});
 
-    describe('#spawn', function () {
-        it('should spawn a child process', function () {
-            assert.ok(deviceIntegration = cp.fork(process.cwd()), 'Child process not spawned.');
-        });
-    });
+	describe('#spawn', function () {
+		it('should spawn a child process', function () {
+			assert.ok(deviceIntegration = cp.fork(process.cwd()), 'Child process not spawned.');
+		});
+	});
 
-    describe('#handShake', function () {
-        it('should notify the parent process when ready within 5 seconds', function (done) {
-            this.timeout(5000);
+	describe('#handShake', function () {
+		it('should notify the parent process when ready within 5 seconds', function (done) {
+			this.timeout(5000);
 
-            deviceIntegration.on('message', function (message) {
-                if (message.type === 'ready')
-                    done();
-            });
+			deviceIntegration.on('message', function (message) {
+				if (message.type === 'ready')
+					done();
+				else if (message.type === 'upsertdevice')
+					console.log(message.data);
+			});
 
-            deviceIntegration.send({
-                type: 'ready',
-                data: {
-                    options: {
-                        username: 'xxxxx',
-                        password: 'xxxxx',
-                        licenseKey: 'xxxxx-xxxx-xxxx'
-                    }
-                }
-            }, function (error) {
-                assert.ifError(error);
-            });
-        });
-    });
+			deviceIntegration.send({
+				type: 'ready',
+				data: {
+					options: {
+						username: 'xxx',
+						password: 'xxx',
+						licenseKey: 'xxx-xxx-xxx-xxx-xxx'
+					}
+				}
+			}, function (error) {
+				assert.ifError(error);
+			});
+		});
+	});
 
-    describe('#sync', function() {
-        it('should sync 3rd party devices to Reekoh devices', function(done) {
-            this.timeout(5000);
-            deviceIntegration.send({
-                type: 'sync',
-                data: {
-                    last_sync_dt: new Date('12-12-1970')
-                }
-            }, done);
-        });
-    });
+	describe('#sync', function () {
+		it('should sync 3rd party devices to Reekoh devices', function (done) {
+			this.timeout(5000);
+
+			deviceIntegration.send({
+				type: 'sync',
+				data: {
+					last_sync_dt: new Date('12-12-1970')
+				}
+			}, () => {
+				setTimeout(done, 4000);
+			});
+		});
+	});
 });
